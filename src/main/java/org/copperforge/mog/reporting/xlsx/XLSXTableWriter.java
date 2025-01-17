5c686b18aa5e8adfc0a40dc07c10b5844c81b79a
Brian Eldridge <brian.eldridge@gainwelltechnologies.com>
2025-01-17 10:50:14 -0600

    updated xslx table writer to be cleaner, better, stronger

--- src/main/java/org/copperforge/mog/reporting/xlsx/XLSXTableWriter.java
+++ src/main/java/org/copperforge/mog/reporting/xlsx/XLSXTableWriter.java
@@ -1,5 +1,6 @@
 package org.copperforge.mog.reporting.xlsx;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.poi.ss.util.AreaReference;
@@ -30,18 +31,17 @@ public class XLSXTableWriter extends XLSXElementWriter<Table> {
             log.debug(String.format("Writing element %s", element));
 
         Table tableElement = (Table) element;
+        XLSXReport xlsxReport = (XLSXReport) report;
 
         // get the data
-        MogDataSource mogDataSource = getDataSource(report, tableElement);
-        if (mogDataSource == null) {
-            if (log.isWarnEnabled())
-                log.warn(String.format("Unable to determine datasource for table '%s'", tableElement.getName()));
-            return;
+        MogDataSource mogDataSource = getDataSource(xlsxReport, tableElement);
+        if (mogDataSource == null && log.isWarnEnabled()) {
+            log.warn(String.format("Unable to determine datasource for table '%s'", tableElement.getName()));
         }
 
         List<? extends MogFetchable> data = (mogDataSource != null)
                 ? mogDataSource.fetch(tableElement.getDataSource().getFilter())
-                : null;
+                : new ArrayList<>();
 
         List<Column> columns = tableElement.getColumns();
         int rowCount = (data != null && !data.isEmpty()) ? data.size() : 1;
@@ -50,93 +50,8 @@ public class XLSXTableWriter extends XLSXElementWriter<Table> {
         if (log.isDebugEnabled())
             log.debug(String.format("rowCount = %d, columnCount = %d", rowCount, columnCount));
 
-        CellReference topLeft = new CellReference(tableElement.getUpperLeft().getRow(),
-                tableElement.getUpperLeft().getCol());
-        CellReference bottomRight = new CellReference(tableElement.getUpperLeft().getRow() + rowCount,
-                tableElement.getUpperLeft().getCol() + columnCount - 1);
-        AreaReference reference = workbook().getCreationHelper().createAreaReference(topLeft, bottomRight);
-
-        // Create
-        XSSFTable table = sheet().createTable(reference);
-        table.setName(tableElement.getName());
-        table.setDisplayName(tableElement.getTitle());
-
-        // For now, create the initial style in a low-level way
-        table.getCTTable().addNewTableStyleInfo();
-        table.getCTTable().getTableStyleInfo().setName(tableElement.getStyle());
-
-        // Style the table
-        // TODO add to report json
-        XSSFTableStyleInfo style = (XSSFTableStyleInfo) table.getStyle();
-        style.setName(tableElement.getStyle());
-        style.setFirstColumn(false);
-        style.setLastColumn(false);
-        style.setShowRowStripes(true);
-        style.setShowColumnStripes(true);
-
-        // reusables
-        XSSFRow row;
-        XSSFCell cell;
-        XSSFTableColumn column;
-        int rowNum = tableElement.getUpperLeft().getRow();
-
-        // headers
-        if (columns != null) {
-            row = sheet().createRow(rowNum++);
-            int colNum = 0;
-            for (Column columnDef : columns) {
-                if (columnDef.getWidth() != null) {
-                    sheet().setColumnWidth(colNum + tableElement.getUpperLeft().getCol(), columnDef.getWidth() * 256);
-                }
-
-                if (log.isDebugEnabled())
-                    log.debug(String.format("table.getColumns() = %d; colNum = %d", table.getColumnCount(), colNum));
-
-                column = table.getColumns().get(colNum);
-                column.setName(columnDef.getTitle());
-                cell = row.createCell(colNum++);
-                cell.setCellValue(columnDef.getTitle());
-            }
-
-            colNum = 0;
-            Object value;
-            if (data != null) {
-                for (MogFetchable reportable : data) {
-                    row = sheet().createRow(rowNum++);
-
-                    colNum = tableElement.getUpperLeft().getCol();
-                    for (Column columnDef : columns) {
-                        cell = row.createCell(colNum++);
-
-                        if (columnDef.getKey() != null) {
-                            value = reportable.get(columnDef.getKey());
-
-                            if (value != null) {
-                                switch (value) {
-                                    case Long longValue -> {
-                                        cell.setCellValue(longValue);
-                                    }
-                                    case Integer intValue -> {
-                                        cell.setCellValue(intValue);
-                                    }
-                                    default -> {
-                                        cell.setCellValue(value != null ? value.toString() : "");
-                                    }
-                                }
-                            } else {
-                                cell.setCellValue("");
-                            }
-                        }
-                    }
-                }
-            } else {
-                row = sheet().createRow(rowNum);
-                for (colNum = 0; colNum <= columnCount; colNum++) {
-                    cell = row.createCell(colNum);
-                    cell.setCellValue("");
-                }
-            }
-        }
+        AreaReference reference = getAreaReference(tableElement, columnCount, rowCount);
+        XSSFTable table = writeTable(xlsxReport, tableElement, reference, columns, data);
 
         if (tableElement.getEnableFilters())
             table.getCTTable().addNewAutoFilter().setRef(reference.formatAsString());
@@ -148,7 +63,7 @@ public class XLSXTableWriter extends XLSXElementWriter<Table> {
      * @param tableElement
      * @return
      */
-    MogDataSource getDataSource(Report report, Table tableElement) {
+    MogDataSource getDataSource(XLSXReport report, Table tableElement) {
         ReportDataSource reportDataSource = tableElement.getDataSource();
         if (reportDataSource == null)
             return null;
@@ -161,4 +76,169 @@ public class XLSXTableWriter extends XLSXElementWriter<Table> {
                 .orElse(null);
     }
 
+    /**
+     * 
+     * @param tableElement
+     * @param reference
+     * @param columns
+     * @param data
+     */
+    XSSFTable writeTable(XLSXReport report, Table tableElement, AreaReference reference, List<Column> columns,
+            List<? extends MogFetchable> data) {
+        XSSFTable table = sheet().createTable(reference);
+        table.setName(tableElement.getName());
+        table.setDisplayName(tableElement.getTitle());
+
+        setTableStyle(report, table, tableElement);
+
+        // reusables
+        int rowNum = tableElement.getUpperLeft().getRow();
+
+        // headers
+        if (columns != null) {
+            writeColumnHeaders(table, tableElement, rowNum++, columns);
+            writeRowData(tableElement, rowNum, columns, data);
+        }
+
+        return table;
+    }
+
+    /**
+     * 
+     * @param table
+     * @param tableElement
+     */
+    void setTableStyle(XLSXReport report, XSSFTable table, Table tableElement) {
+        String styleName = tableElement.getStyle();
+        if (styleName == null)
+            return;
+
+        XLSXStyle xlsxStyle = report.getStyles().stream().filter(s -> s.getName().equals(styleName)).findFirst()
+                .orElse(null);
+        if (xlsxStyle instanceof XLSXTableStyle tableStyle) {
+            table.getCTTable().addNewTableStyleInfo();
+            table.getCTTable().getTableStyleInfo().setName(styleName);
+
+            // Style the table
+            XSSFTableStyleInfo style = (XSSFTableStyleInfo) table.getStyle();
+            style.setName(styleName);
+            style.setFirstColumn(tableStyle.getShowFirstColumn());
+            style.setLastColumn(tableStyle.getShowLastColumn());
+            style.setShowRowStripes(tableStyle.getShowRowStripes());
+            style.setShowColumnStripes(tableStyle.getShowColumnStripes());
+        }
+
+    }
+
+    /**
+     * 
+     */
+    void writeColumnHeaders(XSSFTable table, Table tableElement, int rowNum, List<Column> columns) {
+        XSSFRow row = sheet().createRow(rowNum);
+        XSSFTableColumn column;
+        XSSFCell cell;
+        int colNum = 0;
+        for (Column columnDef : columns) {
+            if (columnDef.getWidth() != null) {
+                sheet().setColumnWidth(colNum + tableElement.getUpperLeft().getCol(), columnDef.getWidth() * 256);
+            }
+
+            if (log.isDebugEnabled())
+                log.debug(String.format("table.getColumns() = %d; colNum = %d", table.getColumnCount(), colNum));
+
+            column = table.getColumns().get(colNum);
+            column.setName(columnDef.getTitle());
+            cell = row.createCell(colNum++);
+            cell.setCellValue(columnDef.getTitle());
+        }
+    }
+
+    /**
+     * 
+     * @param tableElement
+     * @param rowNum
+     * @param columns
+     * @param data
+     */
+    void writeRowData(Table tableElement, int rowNum, List<Column> columns, List<? extends MogFetchable> data) {
+        if (data != null) {
+            for (MogFetchable reportable : data) {
+                writeRow(tableElement, rowNum++, columns, reportable);
+            }
+        } else {
+            int columnCount = columns != null ? columns.size() : 0;
+            XSSFCell cell;
+            XSSFRow row = sheet().createRow(rowNum);
+            for (int colNum = 0; colNum <= columnCount; colNum++) {
+                cell = row.createCell(colNum);
+                cell.setCellValue("");
+            }
+        }
+
+    }
+
+    /**
+     * 
+     * @param tableElement
+     * @param columnCount
+     * @param rowCount
+     * @return
+     */
+    AreaReference getAreaReference(Table tableElement, int columnCount, int rowCount) {
+        CellReference topLeft = new CellReference(tableElement.getUpperLeft().getRow(),
+                tableElement.getUpperLeft().getCol());
+        CellReference bottomRight = new CellReference(tableElement.getUpperLeft().getRow() + rowCount,
+                tableElement.getUpperLeft().getCol() + columnCount - 1);
+        return workbook().getCreationHelper().createAreaReference(topLeft, bottomRight);
+    }
+
+    /**
+     * 
+     * @param tableElement
+     * @param rowNum
+     * @param columns
+     * @param reportable
+     * @return
+     */
+    XSSFRow writeRow(Table tableElement, int rowNum, List<Column> columns, MogFetchable reportable) {
+        XSSFRow row = sheet().createRow(rowNum);
+        int colNum = tableElement.getUpperLeft().getCol();
+        for (Column columnDef : columns) {
+            writeColumn(row, colNum++, columnDef, reportable);
+        }
+        return row;
+    }
+
+    /**
+     * 
+     * @param row
+     * @param colNum
+     * @param columnDef
+     * @param reportable
+     * @return
+     */
+    XSSFCell writeColumn(XSSFRow row, int colNum, Column columnDef, MogFetchable reportable) {
+        XSSFCell cell = row.createCell(colNum);
+
+        if (columnDef.getKey() != null) {
+            Object value = reportable.get(columnDef.getKey());
+
+            if (value != null) {
+                switch (value) {
+                  case Long longValue -> {
+                      cell.setCellValue(longValue);
+                  }
+                  case Integer intValue -> {
+                      cell.setCellValue(intValue);
+                  }
+                  default -> {
+                      cell.setCellValue(value.toString());
+                  }
+                }
+            } else {
+                cell.setCellValue("");
+            }
+        }
+        return cell;
+    }
 }
